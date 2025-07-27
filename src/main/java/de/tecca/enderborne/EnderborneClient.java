93d8c1e92b3d5f6d1b143650965ea00a5d886a06
TheYIMIR <theyimir@gmail.com>
2025-07-27 23:54:23 +0200

    Initial commit

---
+++ src/main/java/de/tecca/enderborne/EnderborneClient.java
@@ -0,0 +1,10 @@
+package de.tecca.enderborne;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderborneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
---
+++ src/main/java/de/tecca/enderborne/EnderborneDataGenerator.java
@@ -0,0 +1,11 @@
+package de.tecca.enderborne;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnderborneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
